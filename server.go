9eea406a49aaa3e6e0a084e2d777215a9ff496a8
Borov <Michael.378826@ad.ccsd.net>
2023-01-13 10:59:10 -0800

    Fixed the basic routing

--- server.go
+++ server.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
