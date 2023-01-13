3aa041d2a9e666cfd797b7b984b0fbf84a9017d2
Borov <Michael.378826@ad.ccsd.net>
2023-01-13 12:13:44 -0800

    Testing out static file serving

--- server.go
+++ server.go
@@ -6,15 +6,14 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	})
-	http.HandleFunc("/joe", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Biden")
-	})
+	fs := http.FileServer(http.Dir("./static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	fmt.Println("Server is running on port 8080")
 
 	// Start server on port specified above
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
