0c3d4a22cd0c338cd163efd1e22a0569d01c6bb2
Borov <Michael.378826@ad.ccsd.net>
2023-01-31 12:14:17 -0800

    Added templating and support for static files.

--- server.go
+++ server.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs.ServeHTTP())
-	//Create handler for test.html to run the file and shit
-	fmt.Println("Server is running on port 8080")
+var tplJoe = template.Must(template.ParseFiles("joe.html"))
+var tplIndex = template.Must(template.ParseFiles("index.html"))
 
-	// Start server on port specified above
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/joe", joeHandler)
+	http.HandleFunc("/", indexHandler)
+
+	//Start server run, files, and other shit.
+	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
 }
+
+func joeHandler(w http.ResponseWriter, r *http.Request) {
+	tplJoe.Execute(w, nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tplIndex.Execute(w, nil)
+}
+
+/*
+	Add error handling and checks to make sure you on the right url (index) before handling.
+*/
