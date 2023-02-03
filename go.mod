4fd211851ad1abd48f1fc6e341de703f953be271
Borov <Michael.378826@ad.ccsd.net>
2023-02-03 12:13:43 -0800

    Hashing Algorithm + Templating Basics Added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main/m
 
 go 1.19
+
+require golang.org/x/crypto v0.5.0
--- server.go
+++ server.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 var tplJoe = template.Must(template.ParseFiles("joe.html"))
 var tplIndex = template.Must(template.ParseFiles("index.html"))
 var errIndex = template.Must(template.ParseFiles("404.html"))
+
+type JoeyB struct {
+	Password string
+	Hash     string
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
@@ -32,10 +39,27 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		tplIndex.Execute(w, nil)
 
 	case "/joe":
-		tplJoe.Execute(w, nil)
+
+		err := tplJoe.Execute(w, JoeyB{"WhiteHouse123", hashPswd("WhiteHouse123")})
+		if err != nil {
+			return
+		}
 
 	default:
 		errIndex.Execute(w, nil)
 
 	}
 }
+
+func hashPswd(pwd string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+/*
+	TODO: Seperate the Handlers for each index.
+	TODO: Abstract many of the proccesses in functions + variables (less inline calls and declarations)
+*/
