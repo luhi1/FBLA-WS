ccc6cefa71e86bc9df6c1c12ce0f9bd30f1451dc
micha <michaelborov2007@gmail.com>
2023-02-05 21:07:42 -0800

    Uh Oh, SQL TIME

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main/m
 
 go 1.19
 
-require golang.org/x/crypto v0.5.0
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	golang.org/x/crypto v0.5.0
+)
--- server.go
+++ server.go
@@ -4,27 +4,33 @@ import (
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
-	"log"
 	"net/http"
+	"strings"
 )
 
-var tplJoe = template.Must(template.ParseFiles("joe.html"))
-var tplIndex = template.Must(template.ParseFiles("index.html"))
-var errIndex = template.Must(template.ParseFiles("404.html"))
-
-type JoeyB struct {
-	Password string
+type UserInformation struct {
+	Username string
 	Hash     string
 }
 
+//As the project develops, determine whether this struct is required.
+//If not, merge into TemplateRequests struct
+
+type TemplateRequests struct {
+	data  UserInformation
+	index string
+}
+
+var tplErr = template.Must(template.ParseFiles("404.html"))
+
+// Start server run, files, and other shit.
 func main() {
 	fs := http.FileServer(http.Dir("static"))
+
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/joe", joeHandler)
+	http.HandleFunc("/", indexHandler)
 
-	http.HandleFunc("/joe", Handler)
-	http.HandleFunc("/", Handler)
-
-	//Start server run, files, and other shit.
 	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -32,34 +38,46 @@ func main() {
 	}
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.String() {
+func tplExec(w http.ResponseWriter, r *http.Request, request TemplateRequests) {
+	var err error = nil
+	temp := template.Must(template.ParseFiles(strings.TrimPrefix(request.index, "/") + ".html"))
 
-	case "/":
-		tplIndex.Execute(w, nil)
+	if r.URL.String() == request.index {
+		err = temp.Execute(w, request.data)
+	} else {
+		err = temp.Execute(w, nil)
+	}
 
-	case "/joe":
-
-		err := tplJoe.Execute(w, JoeyB{"WhiteHouse123", hashPswd("WhiteHouse123")})
-		if err != nil {
-			return
-		}
-
-	default:
-		errIndex.Execute(w, nil)
-
+	if err != nil {
+		return
 	}
 }
 
+func joeHandler(w http.ResponseWriter, r *http.Request) {
+	fullRequest := TemplateRequests{
+		UserInformation{"abcde", hashPswd("abcde")},
+		"/joe",
+	}
+	tplExec(w, r, fullRequest)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fullRequest := TemplateRequests{
+		UserInformation{},
+		"/index",
+	}
+	tplExec(w, r, fullRequest)
+}
+
 func hashPswd(pwd string) string {
+
 	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
 	if err != nil {
-		log.Println(err)
+		return ""
 	}
 	return string(hash)
 }
 
 /*
-	TODO: Seperate the Handlers for each index.
-	TODO: Abstract many of the proccesses in functions + variables (less inline calls and declarations)
+	TODO: It's SQL Time :)
 */
