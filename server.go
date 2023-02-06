a343ff0bb85b62a492b0a83a60601674054370b0
luhi1 <100657351+luhi1@users.noreply.github.com>
2023-02-06 14:00:12 -0800

    Bye Joe

--- server.go
+++ server.go
@@ -9,10 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
-	http.HandleFunc("/joe", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Biden")
-	})
-
+	
 	fmt.Println("Server is running on port 8080")
 
 	// Start server on port specified above
